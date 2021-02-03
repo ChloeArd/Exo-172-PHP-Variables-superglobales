40260285283eb1634137f0835cc0c1221367c5e5
ChloeArd <chloe.ardoise@gmail.com>
2021-02-03 10:17:23 +0100

    Exercice 172

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exo 172</title>
+</head>
+<body>
+
+<?php
+    echo $_SERVER["HTTP_USER_AGENT"];
+    echo $_SERVER["REMOTE_ADDR"];
+    echo $_SERVER["SERVER_NAME"];
+?>
+
+</body>
+</html>
